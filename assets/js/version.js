4ead0c3232818fb29d987db80072121c7dd8653f
Coco <58035280+bwins0668@users.noreply.github.com>
2026-06-13 22:52:26 +0900

    chore: release bookmarks sync web cache update

--- assets/js/version.js
+++ assets/js/version.js
@@ -5,10 +5,10 @@
   window.STUDY_TOOLS_VERSION = {
     webVersion: "v2026.6.13",
     desktopVersion: "v2026.6.13",
-    releaseUrl: "https://github.com/bwins0668/it-study-tools/releases/tag/v2026.6.13-r19.4",
+    releaseUrl: "https://github.com/bwins0668/it-study-tools/releases/tag/v2026.6.13-r19.6",
     webUrl: "https://study-tools-web-pages.pages.dev",
     stage: "stable",
-    assetVersion: "v2026.6.13-r19.4"
+    assetVersion: "v2026.6.13-r19.6"
   };
 
   function applyVersionInfo() {
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "study-tools-web-v2026-6-13-r19-4";
+﻿const CACHE_NAME = "study-tools-web-v2026-6-13-r19-6";
 
 const CORE_ASSETS = [
   "./",
